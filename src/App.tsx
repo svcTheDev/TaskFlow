8d764938b362d9fe1dd2a53722f4a3ebc8609ef5
Sergio Vargas <svargas.ext.uber@gmail.com>
2026-02-06 11:43:43 -0500

    installed components from schadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <body className="bg-black">
+  <h1 className="text-3xl font-bold underline text-white">
+    Hello world!
+  </h1>
+  </body>
+  )
+}
+
+export default App
